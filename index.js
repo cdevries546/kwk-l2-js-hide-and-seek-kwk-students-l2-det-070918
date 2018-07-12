94a379b7524063fe954b9494b968c7dcb3a0b4e0
cdevries546 <cdevries@comcast.net>
2018-07-12 18:08:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ function increaseRankBy (n) {
 }
 
 function deepestChild() {
-  return document.querySelector("#grand-node div div div div div")
-  
+  return document.querySelector("#grand-node div div div div")
 }
 
 
